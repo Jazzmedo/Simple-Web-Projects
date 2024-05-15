7739aefbc4f4a9ba1ceedde3b5ad95c595e4f4c7
JazzMedo <mohamedmelawany@hotmail.com>
2024-05-15 23:02:44 +0300

    2.2

--- 02.Progress-Steps/script.js
+++ 02.Progress-Steps/script.js
@@ -1,6 +1,7 @@
-var hrr = document.querySelectorAll('hr');
 var cons = document.querySelectorAll('.con');
+var prog=document.getElementById('progress');
 
+console.log(prog);
 
 var back = document.querySelector('.btn.back');
 var front = document.querySelector('.btn.front');
@@ -15,14 +16,22 @@ function updatee() {
         back.setAttribute('disabled', "");
         front.classList.add('actbtn');
         front.removeAttribute('disabled');
+        prog.style.width="0%";
     }
     else if (act > 0 && act < 3) {
+        if(act==1){
+            prog.style.width="33%";
+        }
+        else if(act==2){
+            prog.style.width="66%";
+        }
         back.classList.add('actbtn');
         front.classList.add('actbtn');
         front.removeAttribute('disabled');
         back.removeAttribute('disabled');
     }
     if (act == 3) {
+        prog.style.width="100%";
         back.removeAttribute('disabled');
         front.setAttribute('disabled', "");
         front.classList.remove('actbtn');
@@ -32,13 +41,11 @@ function updatee() {
 
 function backClick() {
     act--;
-    hrr[act].classList.remove('actlin');
     cons[act + 1].classList.remove('actcon');
     updatee();
 }
 
 function frontClick() {
-    hrr[act].classList.add('actlin');
     cons[act + 1].classList.add('actcon');
     act++;
     updatee();
